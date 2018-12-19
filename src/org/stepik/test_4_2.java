69fdd6d1dbdbf812a6b23ca97739cc308428dc79
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2018-12-19 15:31:42 +0900

    Finish  in 4.2

---
+++ src/org/stepik/test_4_2.java
@@ -0,0 +1,62 @@
+package org.stepik;
+
+public class test_4_2 {
+
+    public static void main(String[] args) {
+        System.out.println(getCallerClassAndMethodName());
+        anotherMethod();
+    }
+
+    private static void anotherMethod() {
+        System.out.println(getCallerClassAndMethodName());
+    }
+
+    public static String getCallerClassAndMethodName() {
+        // ...
+//        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+//        String message = "";
+//        if(stackTraceElements.length >= 3) {
+//            StackTraceElement element = stackTraceElements[2];
+//            String className = element.getClassName();
+//            String methodName = element.getMethodName();
+//            message = className + "#" + methodName;
+//        }
+//        for (StackTraceElement st: stackTraceElements ) {
+//            System.out.println( st.);
+//        }
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        try {
+            StackTraceElement element = stackTraceElements[3];
+            return  element.getClassName() + "#" + element.getMethodName();
+        }
+        catch (ArrayIndexOutOfBoundsException e
+        ){
+            return null;
+        }
+
+    }
+
+
+
+    public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
+        // your implementation here
+        boolean noStop = true;
+        int count = 0;
+        while (noStop) {
+            try (RobotConnection robotConection = robotConnectionManager.getConnection()) {
+                robotConection.moveRobotTo(toX, toY);
+                noStop = false;
+            } catch (RobotConnectionException e) {
+
+                count++;
+                if (count >= 3) {
+                    noStop = false;
+                    throw new RobotConnectionException("Can not connect robot");
+
+                }
+            }
+        }
+    }
+
+
+    }
