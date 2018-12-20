5cbef63872fdd5ea954679585925e8db6523ab03
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2018-12-20 23:35:37 +0900

    Procces 4.3.2 before implemets class Inspector

--- src/org/stepik/test_4_3_2/Spy.java
+++ src/org/stepik/test_4_3_2/Spy.java
@@ -1,8 +1,29 @@
 package org.stepik.test_4_3_2;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Spy implements MailService {
+    private static final Logger logger = Logger.getLogger(MailService.class.getName());
+
+    public Spy() {
+        logger.setLevel(Level.ALL);
+        Handler handler = new ConsoleHandler();
+        handler.setLevel(Level.ALL);
+        logger.addHandler(handler);
+    }
+
     @Override
     public Sendable processMail(Sendable mail) {
-        return null;
+        if ( mail instanceof MailMessage){
+            if ( mail.getTo().equals("Austin Powers") || mail.getFrom().equals("Austin Powers")){
+                logger.log(Level.WARNING,"Detected target mail correspondence: from {from} to {to} \"{message}\"",mail.getFrom(),mail.getTo(),((MailMessage) mail).getMessage());
+            } else {
+                logger.log(Level.INFO,"Usual correspondence: from {from} to {to}",mail.getFrom(),mail.getTo());
+            }
+        }
+        return mail;
     }
 }
--- src/org/stepik/test_4_3_2/Theif.java
+++ src/org/stepik/test_4_3_2/Theif.java
@@ -1,8 +1,28 @@
 package org.stepik.test_4_3_2;
 
 public class Theif implements  MailService {
+    private  final int minPrice;
+    private int Summ;
+    public Theif(int minPrice ) {
+        this.minPrice = minPrice;
+        this.Summ = 0;
+    }
+
     @Override
     public Sendable processMail(Sendable mail) {
-        return null;
+        if ( mail instanceof MailPackage){
+            MailPackage mailPackage = (MailPackage) mail;
+            Package mPackage = mailPackage.getContent();
+            if ( mPackage.getPrice() > minPrice) {
+                Summ += mPackage.getPrice();
+                Package newPackage = new Package("stones instead of " + ((Package) mail).getContent(),0);
+                return new MailPackage(mail.getFrom(),mail.getTo(),Package);
+            }
+        }
+        return mail;
+    }
+
+    public int getStolenValue(){
+        return Summ;
     }
 }
