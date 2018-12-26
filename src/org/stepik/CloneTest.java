17a09794ce9b8295f13ff0fcae326392536ce87d
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2018-12-26 17:53:09 +0900

    add request for module 5

---
+++ src/org/stepik/CloneTest.java
@@ -0,0 +1,73 @@
+package org.stepik;
+
+import java.io.*;
+
+public class CloneTest {
+    static class Person implements Serializable {
+        String name;
+        int age;
+        Car car;
+
+        Person(Car car, int age, String name) {
+            this.car = car;
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return this.name + " {" +
+                    "age=" + age +
+                    ", car=" + car +
+                    '}';
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+    }
+
+    static class Car implements Serializable {
+        public String color;
+
+        Car(String color) {
+            this.color = color;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "color car='" + color + '\'' +
+                    '}';
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Car car = new Car("Green");
+        Person person = new Person(car, 25, "Mike");
+        //Clone with Serialization
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(bos);
+        objectOutputStream.writeObject(person);
+        objectOutputStream.close();
+        byte[] serializedPersontoByte = bos.toByteArray();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bos.toByteArray());
+        Person clonePersone = null;
+        //Serialize it
+        ObjectInputStream ios = new ObjectInputStream(byteArrayInputStream);
+        clonePersone = (Person) ios.readObject();
+        byte[] byteArr = new byte[bos.toByteArray().length];
+        byteArrayInputStream.read(byteArr);
+
+        System.out.println(person);
+        System.out.println(clonePersone);
+        System.out.println("Меняем клонов выводим");
+        clonePersone.name = new String("Ivan");
+        clonePersone.age = 18;
+        clonePersone.car.color = "red";
+        System.out.println(person);
+        System.out.println(clonePersone);
+    }
+}
--- src/org/stepik/module_5_3_2/Main.java
+++ src/org/stepik/module_5_3_2/Main.java
@@ -1,22 +1,17 @@
 package org.stepik.module_5_3_2;
 
-import java.nio.channels.Channel;
 import java.util.Scanner;
-
 
 public class Main {
 
     public static void main(String[] args) {
-        String subStr;
         double summ = 0;
-
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
-            subStr = scanner.next();
-            try {
-                summ += Double.parseDouble(subStr);
-            } catch (NumberFormatException e) {
-                //ignore
+            if ( scanner.hasNextDouble() ){
+                summ += scanner.nextDouble();
+            } else {
+                scanner.next();
             }
         }
         System.out.printf("%.6f", summ);
