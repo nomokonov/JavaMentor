d83d08ecc266e66801f03bfb4a3610d384f3a43b
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2019-01-08 22:18:33 +0900

    Starting of 6.4.3

--- src/org/stepik/module_6/test_6_4_3/Main.java
+++ src/org/stepik/module_6/test_6_4_3/Main.java
@@ -3,13 +3,24 @@ package src.org.stepik.module_6.test_6_4_3;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.Arrays.stream;
 
 public class Main {
     public static void main(String[] args) {
+        Comparator<Map.Entry<String, Integer>> c1 = Comparator.comparing(Map.Entry::getValue, Comparator.reverseOrder());
+        Comparator<Map.Entry<String, Integer>> c2 = Comparator.comparing(Map.Entry::getKey);
+
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
         Stream<String> stream = bufferedReader.lines();
+        stream.flatMap(x -> stream(x.split("\\W+")))
+                .map(String::toLowerCase)
 
-
+                .forEach(System.out::println);
     }
 }
