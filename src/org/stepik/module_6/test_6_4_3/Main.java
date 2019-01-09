f10e77a4cdd132dd5b8b276633ca0b66e57cf787
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2019-01-09 17:47:34 +0900

    asdas

--- src/org/stepik/module_6/test_6_4_3/Main.java
+++ src/org/stepik/module_6/test_6_4_3/Main.java
@@ -6,21 +6,24 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.Arrays.stream;
-
 public class Main {
     public static void main(String[] args) {
-        Comparator<Map.Entry<String, Integer>> c1 = Comparator.comparing(Map.Entry::getValue, Comparator.reverseOrder());
-        Comparator<Map.Entry<String, Integer>> c2 = Comparator.comparing(Map.Entry::getKey);
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
         Stream<String> stream = bufferedReader.lines();
-        stream.flatMap(x -> stream(x.split("\\W+")))
+        stream
+                .map(x -> x.split("[^a-zA-Zа-яА-Я0-9']+"))
+                .flatMap(Arrays::stream)
                 .map(String::toLowerCase)
-
-                .forEach(System.out::println);
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .forEach(s -> System.out.println(s.getKey()+ " - " + s.getValue()));
+//                .forEach(System.out::println);
     }
 }
