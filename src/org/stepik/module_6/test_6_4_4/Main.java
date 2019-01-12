edbfb6013d3bc2c8d209bd6637b95aaac8d8b76f
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2019-01-13 02:19:12 +0900

    Finishing of base course the Stepik.com

--- src/org/stepik/module_6/test_6_4_4/Main.java
+++ src/org/stepik/module_6/test_6_4_4/Main.java
@@ -1,14 +1,10 @@
 package org.stepik.module_6.test_6_4_4;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
-
 
 
         // Random variables
@@ -23,9 +19,9 @@ public class Main {
                 "This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!"
         );
 
-        assert firstMessage.getFrom().equals("Robert Howard"): "Wrong firstMessage from address";
-        assert firstMessage.getTo().equals("H.P. Lovecraft"): "Wrong firstMessage to address";
-        assert firstMessage.getContent().endsWith("Howard!"): "Wrong firstMessage content ending";
+        assert firstMessage.getFrom().equals("Robert Howard") : "Wrong firstMessage from address";
+        assert firstMessage.getTo().equals("H.P. Lovecraft") : "Wrong firstMessage to address";
+        assert firstMessage.getContent().endsWith("Howard!") : "Wrong firstMessage content ending";
 
         MailMessage secondMessage = new MailMessage(
                 "Jonathan Nolan",
@@ -57,16 +53,16 @@ public class Main {
                 Arrays.asList(
                         "This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!"
                 )
-        ): "wrong mailService mailbox content (1)";
+        ) : "wrong mailService mailbox content (1)";
 
         assert mailBox.get("Christopher Nolan").equals(
                 Arrays.asList(
                         "Брат, почему все так хвалят только тебя, когда практически все сценарии написал я. Так не честно!",
                         "Я так и не понял Интерстеллар."
                 )
-        ): "wrong mailService mailbox content (2)";
+        ) : "wrong mailService mailbox content (2)";
 
-        assert mailBox.get(randomTo).equals(Collections.<String>emptyList()): "wrong mailService mailbox content (3)";
+        assert mailBox.get(randomTo).equals(Collections.<String>emptyList()) : "wrong mailService mailbox content (3)";
 
 
 // Создание списка из трех зарплат.
@@ -83,32 +79,39 @@ public class Main {
 // Получение и проверка словаря "почтового ящика",
 //   где по получателю можно получить список зарплат, которые были ему отправлены.
         Map<String, List<Integer>> salaries = salaryService.getMailBox();
-        assert salaries.get(salary1.getTo()).equals(Arrays.asList(1)): "wrong salaries mailbox content (1)";
-        assert salaries.get(salary2.getTo()).equals(Arrays.asList(Integer.MAX_VALUE)): "wrong salaries mailbox content (2)";
-        assert salaries.get(randomTo).equals(Arrays.asList(randomSalary)): "wrong salaries mailbox content (3)";
+        assert salaries.get(salary1.getTo()).equals(Arrays.asList(1)) : "wrong salaries mailbox content (1)";
+        assert salaries.get(salary2.getTo()).equals(Arrays.asList(Integer.MAX_VALUE)) : "wrong salaries mailbox content (2)";
+        assert salaries.get(randomTo).equals(Arrays.asList(randomSalary)) : "wrong salaries mailbox content (3)";
     }
 
-    interface Sendable {
-        String getTo ();
-        String getFrom();
+    interface Sendable<T> {
+        String getTo();
+
+        T getContext();
     }
 
-    public static class MailMessage implements  Sendable{
+    public static class MailMessage implements Sendable<String> {
         private String from;
         private String to;
         private String text;
+
         public MailMessage(String from, String to, String text) {
             this.from = from;
             this.to = to;
             this.text = text;
         }
 
-        public String getFrom() {
-            return from;
-        }
-
+        @Override
         public String getTo() {
             return to;
+        }
+
+        @Override
+        public String getContext() {
+            return text;
+        }
+        public String getFrom(){
+            return from;
         }
 
         public String getContent() {
@@ -116,7 +119,7 @@ public class Main {
         }
     }
 
-    public static class Salary  {
+    public static class Salary implements Sendable<Integer> {
         private String company;
         private String name;
         private int salary;
@@ -127,22 +130,47 @@ public class Main {
             this.salary = salary;
         }
 
+        @Override
         public String getTo() {
             return name;
         }
+
+        @Override
+        public Integer getContext() {
+            return salary;
+        }
     }
 
-    public static class MailService<T> implements Consumer<Sendable> {
+    public static class MailService<T> implements Consumer<Sendable<T>> {
         Map<String, List<T>> map;
 
-        public Map<String,List<T>> getMailBox(){
+        public MailService() {
+            Map<String, List<T>> map = new MyMap<>();
+            this.map = map;
+        }
+
+        public Map<String, List<T>> getMailBox() {
             return map;
         }
 
+        @Override
+        public void accept(Sendable<T> sendable) {
+            if (map.containsKey(sendable.getTo())) {
+                map.get(sendable.getTo()).add(sendable.getContext());
+            } else {
+                List<T> list = new ArrayList(Arrays.asList(sendable.getContext()));
+                 map.put(sendable.getTo(),list);
+            }
+        }
+
+    }
+
+    static class MyMap<K,V> extends HashMap<K,V>{
 
         @Override
-        public void accept(Sendable sendable) {
-            map.put(sendable.getTo(),sendable.getFrom())
+        public V get(Object key) {
+            List<String> list = new ArrayList<>();
+            return super.getOrDefault(key , (V) list);
         }
     }
 
